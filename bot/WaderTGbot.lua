4830ee3e59d584d1ca115a0ca7f29ebcce1739f0
taha560 <taha102050@gmail.com>
2016-05-26 00:58:58 -0700

    Update WaderTGbot.lua

--- bot/WaderTGbot.lua
+++ bot/WaderTGbot.lua
@@ -249,7 +249,7 @@ function create_config( )
 	"voice",
 	"wai"
     },
-    sudo_users = {67559249,71951412,154868817,143531868,0,tonumber(our_id)},--Sudo users
+    sudo_users = {189308877,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[WaderTG v4
 An advanced administration bot based on TG-CLI written in Lua
@@ -478,10 +478,10 @@ will return group ban list
 
 *Only owner can use res,setowner,promote,demote and log commands
 
-channel:@WaderTGTeam
+channel:
 ]],
 	help_text_super =[[
-WaderTG SuperGroup Commands :
+ SuperGroup Commands :
 =========================
 #info
 About the SuperGroup info
@@ -659,7 +659,7 @@ To show office user
 Text-to-voice
 =========================
 *Only from markes "!" , "/" , "#" use*
-Channel:@WaderTGTeam
+Channel:
 ]],
   }
   serialize_to_file(config, './data/config.lua')
